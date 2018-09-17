1ba447c51af2bcdcd37cb5975c799b7ba4a95d59
Alex Graves <graves@thedp.com>
2018-09-17 17:26:09 -0400

    update for python3, add .gitignore

--- geocoder.py
+++ geocoder.py
@@ -2,8 +2,8 @@ import os
 import sys
 import pandas as pd
 import googlemaps
-import urllib2 as url
 import json
+from urllib.request import urlopen
 
 # change city and state if this is for anywhere other than Philly
 api_key = os.environ['API_KEY']
@@ -27,20 +27,27 @@ data['lng'] = 0.0
 # use Google's geocoding API to get lat/lng from addresses and update data
 for index, row in data.iterrows():
     address = row['address']
+
+    # if address is unspecified, just put 0/0 for its lat/lng
+    if address != address:
+        data.at[index, 'lat'] = 0.0
+        data.at[index, 'lng'] = 0.0
+        continue
+
     address = address.replace(' ', '+')
     address += '+' + city + '+' + state
     api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address, api_key)
 
-    response = url.urlopen(api_url)
+    response = urlopen(api_url)
     result = json.load(response)
 
     if result['results']:
         lat = result['results'][0]['geometry']['location']['lat']
         lng = result['results'][0]['geometry']['location']['lng']
-        data.set_value(index, 'lat', lat)
-        data.set_value(index, 'lng', lng)
+        data.at[index, 'lat'] = lat
+        data.at[index, 'lng'] = lng
     else:
-        data.set_value(index, 'lat', 0.0)
-        data.set_value(index, 'lng', 0.0)
+        data.at[index, 'lat'] = 0.0
+        data.at[index, 'lng'] = 0.0
 
 data.to_csv(output_file, index=False, encoding='utf8')
