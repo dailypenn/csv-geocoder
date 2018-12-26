7aa776987059f5bc1996dec7ab08db71bb3e32e9
Alex Graves <graves@thedp.com>
2018-12-26 09:57:02 -0500

    update to python 3, encode address to fix #1

--- geocoder.py
+++ geocoder.py
@@ -2,8 +2,9 @@ import os
 import sys
 import pandas as pd
 import googlemaps
-import urllib2 as url
 import json
+from urllib.request import urlopen
+from urllib.parse import quote
 
 # change city and state if this is for anywhere other than Philly
 api_key = os.environ['API_KEY']
@@ -29,18 +30,19 @@ for index, row in data.iterrows():
     address = row['address']
     address = address.replace(' ', '+')
     address += '+' + city + '+' + state
+    address = quote(address) # encode address for special characters
     api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address, api_key)
 
-    response = url.urlopen(api_url)
+    response = urlopen(api_url)
     result = json.load(response)
 
     if result['results']:
         lat = result['results'][0]['geometry']['location']['lat']
         lng = result['results'][0]['geometry']['location']['lng']
-        data.set_value(index, 'lat', lat)
-        data.set_value(index, 'lng', lng)
+        data.at[index, 'lat'] = lat
+        data.at[index, 'lng'] = lng
     else:
-        data.set_value(index, 'lat', 0.0)
-        data.set_value(index, 'lng', 0.0)
+        data.at[index, 'lat'] = 0.0
+        data.at[index, 'lng'] = 0.0
 
 data.to_csv(output_file, index=False, encoding='utf8')
